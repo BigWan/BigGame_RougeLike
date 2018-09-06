d13f2a64bf4ff51de02fa08f2b6c1f60884e637b
BigWan <BigWan@users.noreply.github.com>
2018-09-06 20:19:06 +0800

    import package
    import Little Heroes Mega Pack

---
+++ BigRouge/Assets/Resources/Avatar/Scripts/BodyAvatar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BigRogue.Avatar {
+
+    public class BodyAvatar : MonoBehaviour {
+
+        [Header("Mount Points")]
+        public Transform basePoint;
+        public Transform headPoint;
+        public Transform backPoint;
+        public Transform leftPoint;
+        public Transform rightPoint;
+
+        [Header("Components Refs")]
+        // 身体的材质球
+        public SkinnedMeshRenderer m_bodyMeshRenderer;
+
+
+        public Material bodyMaterial {
+            get {
+                return m_bodyMeshRenderer.material;
+            }
+            set {
+                m_bodyMeshRenderer.material = value;
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
