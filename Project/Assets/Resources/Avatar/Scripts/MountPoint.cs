87b3fb8abcd366723886a14123bc6ab2e7965bee
Bigwan <BigWan@users.noreply.github.com>
2018-09-06 17:25:00 +0800

    Init

---
+++ Project/Assets/Resources/Avatar/Scripts/MountPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BigRogue.Avatar {
+
+    public enum MountPointType {
+        Base,Head,Back,Left,Right,BothHand
+    }
+
+    /// <summary>
+    /// 模型上面的挂点
+    /// </summary>
+    public class MountPoint : MonoBehaviour {
+
+        public MountPointType mountPointType;
+
+    }
+}
